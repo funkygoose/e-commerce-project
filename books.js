9e5509b67a335cd4439f152b186740c242ba16ca
Uriel Banuelos <uriel1119@gmail.com>
2022-09-13 11:59:00 -0700

    initial commit

--- books.js
+++ books.js
@@ -1,18 +1,15 @@
 function renderBooks() {
-  const booksWrapper = document.querySelector('.books');
-
-  console.log(getBooks())
+  const booksWrapper = document.querySelector(".books");
 
   const books = getBooks();
-  console.log(books)
-  booksWrapper.innerHTML = 
-      `<div class="book">
+
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
       <figure class="book__img--wrapper">
-        <img class="book__img" src="${books[0].url}" alt="">
+        <img class="book__img" src="${book.url}" alt="">
       </figure>
       <div class="book__title">
-        ${books[0].title}
-        Atomic Habits
+        ${book.title}
       </div>
       <div class="book__ratings">
         <i class="fas fa-star"></i>
@@ -22,13 +19,20 @@ function renderBooks() {
         <i class="fas fa-star-half-alt"></i>
       </div>
       <div class="book__price">
-        <span class="book__price--normal">$${books[0].originalPrice}</span> $${books[0].salePrice}
+        <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
       </div>
-    </div>`
+    </div>`;
+  });
+
+  console.log(booksHtml)
+
+  booksWrapper.innerHtml = booksHtml;
+  //booksWrapper.innerHTML = 
 }
+
 setTimeout(() => {
   renderBooks();
-},);
+});
 
 // FAKE DATA
 function getBooks() {
@@ -36,7 +40,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
